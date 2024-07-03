b3bc224c9619f6a1cfbf04d166f6dafc36687166
henriquepadua <henrique.lommes@gmail.com>
2024-07-03 12:34:20 -0300

    metodo criar participante funcional

--- eventos/lib/Controllers/Participante/CriarParticipanteController.dart
+++ eventos/lib/Controllers/Participante/CriarParticipanteController.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class CriarParticipanteController {
   Future<bool> criandoParticipante(int id, String email, String nome, bool ativo) async {
-    const String apiUrl = "https://localhost:7148/api/Participante/CriarParticipante";
+    const String apiUrl = "https://localhost:7148/api/Participante/CriaParticipante";
 
     final Map<String, String> headers = {
       'Content-Type': 'application/json; charset=UTF-8',
